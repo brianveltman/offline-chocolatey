174cc5ab8a3fd5585239a9c6845834c4cde8ad65
Brian Veltman <brian.veltman@cz.nl>
2020-10-30 10:33:02 +0100

    add spec and ps1

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+. $toolsPath\helpers.ps1
+
+$version = '86.0.4240.111'
+if ($version -eq (Get-ChromeVersion)) {
+  Write-Host "Google Chrome $version is already installed."
+  return
+}
+
+$packageArgs = @{
+  packageName            = 'googlechrome'
+  fileType               = 'MSI'
+  url                    = '$toolsPath/googlechromestandaloneenterprise.msi'
+  url64bit               = '$toolsPath/googlechromestandaloneenterprise64.msi'
+  checksum               = 'cba09bf022f5c2e3da187951ee837b7f8c9772ee6c06a6236fa2dbeb26532f9b'
+  checksum64             = 'a01f61b4bcdbd7f1469b27a76f22c1d0e155e2a596e1b469a4dcfbee3fb5994d'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes         = @(0)
+}
+
+if (Get-Chrome32bitInstalled) { 'url64bit', 'checksum64', 'checksumType64' | ForEach-Object { $packageArgs.Remove($_) } }
+Install-ChocolateyPackage @packageArgs
